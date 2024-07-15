fc04c2f672873e0ca91fa8faa05a5b4585b4db52
sai_vara_1729 <144797272+saivara1341@users.noreply.github.com>
2024-07-15 12:51:34 +0530

    script.js

--- script.js
+++ script.js
@@ -68,9 +68,12 @@ function isMobileDevice() {
     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
 if (isMobileDevice()) {
-    createLetters(" ⟩ Udhay ⟩ Udhay", 124, 0, 'white' , 'forwards', 'set1');
+    createLetters(" ⟩ Azeem ⟩ Azeem", 124, 0, 'white' , 'forwards', 'set1');
+    createLetters(" ⟨ Happy Birthday ⟨ Happy Birthday ⟨ Happy Birthday", 200, 0, 'white', 'backwards','set2');
+}
+
 } else {
-    createLetters(" ⟩ Sahiti ⟩ Sahiti", 124, 0, 'white' , 'forwards','set1');
+    createLetters(" ⟩ Azeem ⟩ Azeem", 124, 0, 'white' , 'forwards','set1');
     createLetters(" ⟨ Happy Birthday ⟨ Happy Birthday ⟨ Happy Birthday", 200, 0, 'white', 'backwards','set2');
 }
 
